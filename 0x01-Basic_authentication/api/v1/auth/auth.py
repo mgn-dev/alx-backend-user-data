6d1d93ef597005a07631c28dd58c1a0fa555cef0
emjay026 <126557401+mgn-dev@users.noreply.github.com>
2024-11-12 23:55:22 +0200

    Do task 4, project 0x01. Basic authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -28,7 +28,13 @@ class Auth:
         Returns:
             bool: True if authentication is required, False otherwise.
         """
-        return False
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        path = path.rstrip('/') + '/'
+        for excluded_path in excluded_paths:
+            if path == excluded_path:
+                return False
+        return True
 
     def authorization_header(self, request: request = None) -> str:
         """
