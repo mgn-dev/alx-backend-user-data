e8f47053cd17a0adf688612d38a0ac43418631de
emjay026 <126557401+mgn-dev@users.noreply.github.com>
2024-11-13 04:14:13 +0200

    Do task 10, project 0x01. Basic authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -92,7 +92,7 @@ class BasicAuth(Auth):
         return decoded_base64_authorization_header.split(":", 1)
 
     def user_object_from_credentials(self, user_email: str,
-                                     user_pwd: str) -> UserType:
+                                     user_pwd: str) -> TypeVar('User'):
         """
         Get the User instance based on the user email and password.
 
