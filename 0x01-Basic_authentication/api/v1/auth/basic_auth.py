7f7c5a00cd0b8d6852c86b140c27db4704b498a2
emjay026 <126557401+mgn-dev@users.noreply.github.com>
2024-11-13 03:16:24 +0200

    Do task 7, project 0x01. Basic authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -15,4 +15,24 @@ class BasicAuth(Auth):
     Currently, this class is empty and serves as a placeholder for
     future implementation.
     """
-    pass
+
+    def extract_base64_authorization_header(self, authorization_header: str)\
+            -> str:
+        """
+        Extract the Base64 part of the Authorization header for Basic
+        Authentication.
+
+        Args:
+            authorization_header (str): The value of the Authorization header.
+
+        Returns:
+            str: The Base64 part of the Authorization header,
+            or None if invalid.
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header.split(" ", 1)[1]
